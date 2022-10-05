71033a26835ef14499e33aa6818b7c75e2d41dc7
Ashok Patel <96565022+ashokpatel574@users.noreply.github.com>
2022-10-05 10:41:09 +0530

    Initial commit

---
+++ Script.js
@@ -0,0 +1,56 @@
+"use strict";
+
+{
+  const dateInput = document.querySelector(".dateInput");
+  const numberInput = document.querySelector(".numberInput");
+  const btnCheck = document.querySelector(".btn-check");
+  const formConatiner = document.querySelector(".form");
+
+  const resultContainer = document.querySelector(".result-container");
+  const result = document.querySelector(".result");
+  const closeResult = document.querySelector(".close-result");
+
+  const privacyNote = setTimeout(() => {
+    document.querySelector(".privacy-note-container").classList.add("show");
+  }, 5000);
+
+  function clickHandler(e) {
+    e.preventDefault();
+
+    if (!dateInput.value || !numberInput.value) {
+      alert("Pleaase enter your Birth date and lucky number");
+    } else {
+      let numberInputValue = parseInt(numberInput.value);
+      const date = dateInput.value.split("-");
+      const dateInputValue = date.reduce(
+        (curr, accum) => parseInt(curr) + parseInt(accum),
+        0
+      );
+
+      clearTimeout(privacyNote);
+      formConatiner.style.display = "none";
+
+      console.log(privacyNote);
+
+      if (dateInputValue % numberInputValue === 0) {
+        resultContainer.style.display = "block";
+        result.innerText = `Congralutions, your Birth date is a lucky date. 🤩🍾`;
+        resultContainer.style.borderColor = "green";
+      } else {
+        resultContainer.style.display = "block";
+        result.innerText = "Oops!!, your Birth date is not a lucky date. 😱😱";
+        resultContainer.style.borderColor = "red";
+      }
+    }
+  }
+
+  function closeResultHandler(e) {
+    formConatiner.style.display = "flex";
+    resultContainer.style.display = "none";
+    dateInput.value = "";
+    numberInput.value = "";
+  }
+
+  btnCheck.addEventListener("click", clickHandler);
+  closeResult.addEventListener("click", closeResultHandler);
+}
